5338a3d5d74c03240e89e6479dd1e45de50106fc
KhailaN <kailanoverisa@gmail.com>
2024-11-26 22:35:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include "header.h"
+
+int main(){
+    playmusic("intro.wav", SND_ASYNC);
+
+  initscr();
+  noecho();
+  cbreak();
+
+  introframe();
+  
+  napms(2000);
+  
+  stopmusic();
+  
+  clear();
+  refresh();
+
+  napms(2000);
+  
+  playmusic("chapter.wav", SND_ASYNC);
+
+  dialog001();
+  refresh();
+  
+  clear();
+  refresh();
+
+  stopmusic();
+
+  napms(2000);
+  
+  playmusic("peaceful.wav");
+
+  dialog002();
+  refresh();
+
+  dialog003();
+  refresh();
+
+  dialog004();
+  refresh();
+
+  dialog005();
+  refresh();
+
+  stopmusic();
+  
+  napms(2000);
+  
+  playmusic("danger.wav");
+
+  dialog006();
+  refresh();
+  
+  battlemodifierA = 'A';
+  battlemodifierB = 'Q';
+  
+  while(specialtracker2==1) {
+
+  dialog007();
+  refresh();
+
+  stopmusic();
+  
+  napms(2000);
+  
+  playmusic("battle.wav");
+  
+  battlesystem();
+  checkbattle();
+
+  if (specialtracker2==1) {
+  stopmusic();
+  playmusic("danger.wav");
+  }
+  else {
+  	stopmusic();
+  }
+  };
+  
+  napms(2000);
+  
+  playmusic("great.wav");
+  
+  dialog008();
+  refresh();
+
+  stopmusic();
+  
+  dialog009();
+  refresh();
+  
+  playmusic("unpleasant.wav");
+  
+  dialog010();
+  refresh();
+
+  stopmusic();
+  
+  clear();
+  refresh();
+
+  napms(2000);
+  
+  playmusic("chapter.wav", SND_ASYNC);
+  
+  dialog011();
+  refresh();
+  
+  clear();
+  refresh();
+  
+  stopmusic();
+
+  napms(2000);
+  
+  playmusic("journey.wav");
+  
+  dialog012();
+  refresh();
+  
+  stopmusic();
+  
+  napms(2000);
+  
+  playmusic("sorrow.wav");
+  
+  dialog013();
+  refresh();
+
+  endwin();
+
+  stopmusic();
+
+  while (true) {
+    Sleep(100);
+    break;
+  }
+
+  return 0;
+}
