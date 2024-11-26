e5347e20ac8fbc583c48c742533b364436d27adc
KhailaN <kailanoverisa@gmail.com>
2024-11-26 22:35:44 +0700

    Add files via upload

---
+++ stat.h
@@ -0,0 +1,18 @@
+int playerhp = 100;
+int playermana = 50;
+int playerattack = 4;
+int playermagic = 8;
+int playerdefense = 2;
+int playermagicdefense = 1;
+
+int enemyhp = 100;
+int enemyattack = 4;
+int enemymagic = 8;
+int enemydefense = 2;
+int enemymagicdefense = 1;
+
+char battlemodifierA;
+char battlemodifierB;
+
+int specialtracker;
+int specialtracker2 = 1;
